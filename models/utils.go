94e4127cd3605dbd74d23b956d36926a40f6edba
David <dheras@xmltravegate.com>
2020-02-24 17:36:39 +0100

    feat: util function to make update query

---
+++ models/utils.go
@@ -0,0 +1,38 @@
+package models
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func makeUpdateQuery(obj interface{}) string {
+
+	rv := reflect.ValueOf(obj)
+	opt := ""
+
+	for i := 0; i < rv.NumField(); i++ {
+
+		if !rv.Field(i).IsZero() {
+			field := rv.Field(i)
+
+			if field.Kind() == reflect.Ptr {
+				field = field.Elem()
+			}
+
+			if i != 0 && opt != "" {
+				opt += ", "
+			}
+
+			switch field.Kind() {
+			case reflect.Int32:
+				opt += fmt.Sprintf("%s= %v", rv.Type().Field(i).Tag.Get("db"), field)
+			case reflect.String:
+				opt += fmt.Sprintf("%s= '%s'", rv.Type().Field(i).Tag.Get("db"), field)
+			default:
+				fmt.Println(field.Kind())
+			}
+		}
+	}
+
+	return opt
+}
