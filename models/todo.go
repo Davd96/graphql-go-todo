e02fabb952ee00b0c8d7065c03aaaf38c73892f0
David <dheras@xmltravegate.com>
2020-02-24 17:37:33 +0100

    feat: added create and update

--- models/todo.go
+++ models/todo.go
@@ -1,18 +1,29 @@
 package models
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type TodoStatus string
+
+type TodoResponse struct {
+	ID          int32      `db:"id"`
+	Description string     `db:"description"`
+	Status      TodoStatus `db:"status"`
+	UserID      int32      `db:"user_id"`
+}
 
 type Todo struct {
-	ID          int32  `db:"id"`
-	Description string `db:"description"`
-	Status      bool   `db:"status"`
-	UserID      int32  `db:"user_id"`
+	ID          int32       `db:"id"`
+	Description *string     `db:"description"`
+	Status      *TodoStatus `db:"status"`
+	UserID      *int32      `db:"user_id"`
 }
 
 type TodoService struct{}
 
-func (c TodoService) GetAll() []Todo {
-	var todo []Todo
+func (c TodoService) GetAll() []TodoResponse {
+	var todo []TodoResponse
 	err := GetConnection().Select(&todo, "SELECT id,description,status,user_id FROM todo;")
 	if err != nil {
 		fmt.Println(err.Error())
@@ -20,8 +31,8 @@ func (c TodoService) GetAll() []Todo {
 	return todo
 }
 
-func (c TodoService) GetById(id int32) []Todo {
-	var todo []Todo
+func (c TodoService) GetById(id int32) []TodoResponse {
+	var todo []TodoResponse
 	err := GetConnection().Select(&todo, fmt.Sprintf("SELECT id,description,status,user_id FROM todo WHERE id = %v", id))
 	if err != nil {
 		fmt.Println(err.Error())
@@ -29,11 +40,36 @@ func (c TodoService) GetById(id int32) []Todo {
 	return todo
 }
 
-func (c TodoService) GetByUserId(id int32) []Todo {
-	var todo []Todo
+func (c TodoService) GetByUserId(id int32) []TodoResponse {
+	var todo []TodoResponse
 	err := GetConnection().Select(&todo, fmt.Sprintf("SELECT id,description,status,user_id FROM todo WHERE user_id = %v", id))
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	return todo
+}
+
+func (c TodoService) Save(todo Todo) *TodoResponse {
+	var insertedTodo TodoResponse
+	query := fmt.Sprintf("INSERT INTO public.todo (description, status, user_id) VALUES ('%s', 'PENDING', %v) returning id, description, status, user_id", *todo.Description, todo.UserID)
+
+	err := GetConnection().QueryRow(query).Scan(&insertedTodo.ID, &insertedTodo.Description, &insertedTodo.Status, &insertedTodo.UserID)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return &insertedTodo
+}
+
+func (c TodoService) Update(todo Todo) *TodoResponse {
+	var updatedTodo TodoResponse
+	query := fmt.Sprintf("UPDATE public.todo SET %s WHERE id = %v returning id, description, status, user_id", makeUpdateQuery(todo), todo.ID)
+	err := GetConnection().QueryRow(query).Scan(&updatedTodo.ID, &updatedTodo.Description, &updatedTodo.Status, &updatedTodo.UserID)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return &updatedTodo
 }
--- models/user.go
+++ models/user.go
@@ -4,19 +4,20 @@ import (
 	"fmt"
 )
 
-type User struct {
+type UserResponse struct {
 	ID   int32  `db:"id"`
 	Name string `db:"name"`
 }
 
-type NewUser struct {
-	Name string
+type User struct {
+	ID   int32   `db:"id"`
+	Name *string `db:"name"`
 }
 
 type UserService struct{}
 
-func (c UserService) GetAll() []User {
-	var user []User
+func (c UserService) GetAll() []UserResponse {
+	var user []UserResponse
 	err := GetConnection().Select(&user, "SELECT id,name FROM public.user")
 	if err != nil {
 		fmt.Println(err.Error())
@@ -24,8 +25,8 @@ func (c UserService) GetAll() []User {
 	return user
 }
 
-func (c UserService) GetById(id int32) []User {
-	var user []User
+func (c UserService) GetById(id int32) []UserResponse {
+	var user []UserResponse
 	err := GetConnection().Select(&user, fmt.Sprintf("SELECT id,name FROM public.user WHERE id = %v", id))
 	if err != nil {
 		fmt.Println(err.Error())
@@ -33,15 +34,31 @@ func (c UserService) GetById(id int32) []User {
 	return user
 }
 
-func (c UserService) Save(user NewUser) *User {
+func (c UserService) Save(user User) *UserResponse {
+	var insertedUser UserResponse
+	query := fmt.Sprintf("INSERT INTO public.user (name) VALUES ('%s') returning id, name", *user.Name)
 
-	_, err := GetConnection().NamedExec(`INSERT INTO public.user (name) VALUES (:name)`,
-		map[string]interface{}{
-			"name": user.Name,
-		})
+	err := GetConnection().QueryRow(query).Scan(&insertedUser.ID, &insertedUser.Name)
+
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
-	return &User{ID: 10, Name: "test"}
+	return &insertedUser
+}
+
+func (c UserService) Update(user User) *UserResponse {
+	var updatedUser UserResponse
+	query := fmt.Sprintf("UPDATE public.user SET %s WHERE id = %v returning id, name", makeUpdateQuery(user), user.ID)
+	err := GetConnection().QueryRow(query).Scan(&updatedUser.ID, &updatedUser.Name)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return &updatedUser
+}
+
+func (c UserService) Delete(user User) {
+
 }
