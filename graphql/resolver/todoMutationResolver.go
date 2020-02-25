7158fe5bd924c8270b1e005698210a0dfad17517
David <dheras@xmltravegate.com>
2020-02-25 13:01:36 +0100

    feat: added delete user and todo mutation

--- graphql/resolver/todoMutationResolver.go
+++ graphql/resolver/todoMutationResolver.go
@@ -6,46 +6,46 @@ import (
 
 type todoMutationResolver struct{}
 
-type CreateTodo struct {
-	Input models.Todo
-}
-
-type UpdateTodo struct {
-	Input models.Todo
-}
-
-type CreateUser struct {
-	Input models.User
-}
-
-type UpdateUser struct {
-	Input models.User
-}
-
-func (_ *todoMutationResolver) CreateTodo(todoInput *CreateTodo) *todoResolver {
-	data := todoService.Save(todoInput.Input)
+func (_ *todoMutationResolver) CreateTodo(todoInput models.CreateTodo) *todoResolver {
+	data := todoService.Save(todoInput)
 	return &todoResolver{
 		todo: *data,
 	}
 }
 
-func (_ *todoMutationResolver) UpdateTodo(todoInput *UpdateTodo) *todoResolver {
-	data := todoService.Update(todoInput.Input)
+func (_ *todoMutationResolver) UpdateTodo(todoInput models.UpdateTodo) *todoResolver {
+	data := todoService.Update(todoInput)
 	return &todoResolver{
 		todo: *data,
 	}
 }
 
-func (_ *todoMutationResolver) CreateUser(userInput *CreateUser) *userResolver {
-	data := userService.Save(userInput.Input)
+func (_ *todoMutationResolver) DeleteTodo(todoInput models.DeleteTodo) *todoResolver {
+
+	data := todoService.Delete(todoInput)
+	return &todoResolver{
+		todo: *data,
+	}
+}
+
+func (_ *todoMutationResolver) CreateUser(userInput models.CreateUser) *userResolver {
+	data := userService.Save(userInput)
 	return &userResolver{
 		user: *data,
 	}
 }
 
-func (_ *todoMutationResolver) UpdateUser(userInput *UpdateUser) *userResolver {
+func (_ *todoMutationResolver) UpdateUser(userInput models.UpdateUser) *userResolver {
 
-	data := userService.Update(userInput.Input)
+	data := userService.Update(userInput)
+	return &userResolver{
+		user: *data,
+	}
+}
+
+func (_ *todoMutationResolver) DeleteUser(userInput models.DeleteUser) *userResolver {
+
+	data := userService.Delete(userInput)
 	return &userResolver{
 		user: *data,
 	}
